4ccec2b30257aa1b600460e4fd8ce7feaefb5db1
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2025-11-19 22:50:54 +0100

    Adding Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte';
+import { inject } from '@vercel/analytics';
 
+inject();
 const app = mount(App, {
   target: document.getElementById('app')!,
 })
